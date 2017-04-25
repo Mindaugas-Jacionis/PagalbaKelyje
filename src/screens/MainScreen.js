aae16b3d4008cc81646c1e9428f953e07b155fa0
Mindaugas Jacionis <m.jacionis@gmail.com>
2017-04-25 10:57:31 +0300

    Adds modal and couple of buttons to main screen

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 class MainScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      carPlate: '',
+      showModal: false
+    }
+  }
+
+  toggleModal() {
+    this.setState({ showModal: !this.state.showModal });
+  }
+
+  nextScreen() {
+    alert('Call help');
   }
 
   render() {
+    const { carPlate, showModal } = this.state;
+
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>
-          MainScreen
-        </Text>
+        <TouchableOpacity
+          style={showModal ? [styles.modalWrapper, styles.activeModal] : styles.modalWrapper}
+          onPress={() => this.toggleModal()}
+        >
+          <View style={styles.modal}>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.text}>Išsaugoti</Text>
+            </TouchableOpacity>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.button, styles.carPlate]}
+          onPress={() => this.toggleModal()}
+        >
+          <Text style={styles.text}>{carPlate || 'Įvesti automobilio numerius'}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.button, styles.help]}
+          onPress={() => this.nextScreen()}
+        >
+          <Text style={[styles.text, { color: '#fff' }]}>Kviesti pagalba</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -23,8 +55,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    marginHorizontal: 14
+    backgroundColor: '#F5FCFF'
   },
 
   text: {
@@ -34,6 +65,52 @@ const styles = StyleSheet.create({
     color: '#000000',
     margin: 10,
   },
+
+  modalWrapper: {
+    position: 'absolute',
+    overflow: 'hidden',
+    width: 0,
+    height: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    justifyContent: 'center',
+    zIndex: 99
+  },
+
+  activeModal: {
+    width: '100%',
+    height: '100%'
+  },
+
+  modal: {
+    paddingHorizontal: 14,
+    margin: 14,
+    backgroundColor: '#ffffff',
+    borderRadius: 6
+  },
+
+  button: {
+    paddingHorizontal: 14,
+    paddingVertical: 7,
+    width: '100%',
+    borderRadius: 6,
+    marginVertical: 14,
+    backgroundColor: '#239d1a'
+  },
+
+  carPlate: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    padding: 7,
+    backgroundColor: '#ffffff',
+    borderWidth: 2,
+    borderColor: '#000000',
+    width: null
+  },
+
+  help: {
+    position: 'absolute',
+    bottom: 14
+  }
 });
 
 export default MainScreen;
